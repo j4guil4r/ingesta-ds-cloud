f9401f8b89f85f037622e030c11ec424ea051cf3
Jose Aguilar <165909913+j4guil4r@users.noreply.github.com>
2024-10-06 21:01:23 -0500

    Update ingest_postgres.py

--- postgres/ingest_postgres.py
+++ postgres/ingest_postgres.py
@@ -4,26 +4,27 @@ import boto3
 
 # Conectar a la base de datos PostgreSQL
 connection = psycopg2.connect(
-    host='3.234.197.222',  # IP de la base de datos PostgreSQL
+    host='3.234.197.222',
     port=8006,  # Puerto expuesto en Docker para PostgreSQL
     user='postgres',
     password='utec',
     database='MS_Cursos'
 )
 
-
-# Consulta para extraer los datos de la tabla Curso
-df = pd.read_sql("SELECT * FROM Curso", connection)
-
-# Guardar los datos como CSV en un archivo temporal
-csv_file = "/tmp/cursos.csv"
-df.to_csv(csv_file, index=False)
-
 # Conectar a S3
 s3 = boto3.client('s3')
-
-# Subir el archivo CSV al bucket S3
 bucket_name = 'ingesta-aguilar-yoyi-ema-cloud-2'  # Reemplaza con el nombre de tu bucket
-s3.upload_file(csv_file, bucket_name, 'microservicio2/cursos.csv')
 
-print("Datos cargados a S3 con éxito.")
+# Consulta para extraer registros de la tabla Curso
+df_cursos = pd.read_sql("SELECT * FROM Curso", connection)
+csv_file_cursos = "/tmp/cursos.csv"
+df_cursos.to_csv(csv_file_cursos, index=False)
+s3.upload_file(csv_file_cursos, bucket_name, 'microservicio2/cursos.csv')
+
+# Consulta para extraer registros de la tabla Profesor
+df_profesores = pd.read_sql("SELECT * FROM Profesor", connection)
+csv_file_profesores = "/tmp/profesores.csv"
+df_profesores.to_csv(csv_file_profesores, index=False)
+s3.upload_file(csv_file_profesores, bucket_name, 'microservicio2/profesores.csv')
+
+print("Datos de Cursos y Profesores cargados a S3 con éxito.")
